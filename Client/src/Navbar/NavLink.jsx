f0cfab0dc08abcfbe829733280081732ffd5604b
Jai Prakash Singh <prakash.jaiisingh@gmail.com>
2024-09-25 22:23:39 +0530

    Nav Links updated
    nav link 'home" will be active  at root  also.

--- Client/src/Navbar/NavLink.jsx
+++ Client/src/Navbar/NavLink.jsx
@@ -3,7 +3,7 @@ import './NavLink.css'
 function NavLink({name}) {
     let href = name.replaceAll(' ','');
   return (
-   <li className='nav-link'><a href={`/${href}`} style={{color: window.location.pathname === `/${href}` ? '#2F80ED' : '#808080'}}>{name}</a></li>
+   <li className='nav-link'><a href={`/${href}`} style={{color: (window.location.pathname === `/${href}`)||(window.location.pathname === '/'&&name === 'Home') ? '#2F80ED' : '#808080'}}>{name}</a></li>
   )
 }
 
